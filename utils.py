317989c3f0ee61a1b43e73aa746dc6475e680c43
Arvitheslayer <89351600+Arvitheslayer@users.noreply.github.com>
2023-02-23 19:39:40 +0200

    Added all functions in utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,53 @@
 from json import load
 
-def get_posts_all():
-    with open('data/posts.json', 'r', encoding='utf-8') as file:
+def get_posts_all(path):
+    with open(path, 'r', encoding='utf-8') as file:
         posts = load(file)
     return posts
+
+def get_posts_by_user(user_name):
+    list_users = []
+    for i in map(lambda x: x['poster_name'], get_posts_all()):
+        list_users.append(i)
+    if user_name not in list_users:
+        raise ValueError
+        return []
+    list_ = []
+    for post in get_posts_all('data/posts.json'):
+        if post['poster_name'] == user_name:
+            list_.append(post)
+    return list_
+
+
+def get_comment_by_id(post_id):
+    list_ids = []
+    for i in map(lambda x: x['post_id'], get_posts_all()):
+        list_ids.append(i)
+    if post_id not in list_ids:
+        raise ValueError
+        return []
+    list_ = []
+    for comment in get_posts_all('data/comments.json'):
+        if comment['post_id'] == post_id:
+            list_.append(comment['comment'])
+    return list_
+
+
+def search_for_posts(query):
+    list_ = []
+    for post in get_posts_all('data/posts.json'):
+        if query in post['content']:
+            list_.append(post)
+    return list_
+
+
+def get_post_by_pk(pk):
+    list_ = []
+    for post in get_posts_all('data/posts.json'):
+        if pk == post['pk']:
+            list_.append(post)
+    return list_
+
+
+
+
